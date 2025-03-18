29b02e7b20250a24546dde1753e4f6f340d554c0
Ludv1k <157578117+Ludv1k@users.noreply.github.com>
2025-03-18 10:55:02 +0100

    removed the color changing, and added a second smaller text area that can be changed

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,5 +1,7 @@
-function changeColor() {
-    let color = document.getElementById("colorInput").value;
-    document.body.style.color = color;
-    document.getElementById("colorText").innerHTML = color;
+function test() {
+    let name = document.getElementById("writeName").value;
+    document.getElementById("name").innerHTML = name;
+
+    let content = document.getElementById("writeContent").value;
+    document.getElementById("content").innerHTML = content;
 }
