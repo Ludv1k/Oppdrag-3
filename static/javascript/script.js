64a0b3a73d6cbba9ef84bd14b4a42a6233ef2974
Ludv1k <157578117+Ludv1k@users.noreply.github.com>
2025-03-19 09:29:42 +0100

    Added three different checkboxes for different background colors

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -5,3 +5,44 @@ function test() {
     let content = document.getElementById("writeContent").value;
     document.getElementById("content").innerHTML = content;
 }
+
+
+var checkBoxes = document.querySelectorAll("input[type='checkbox']");
+
+checkBoxes.forEach((box) => {
+    box.addEventListener("change", function() {
+        checkBoxes.forEach((otherBox) => {
+            if (otherBox !== this) {
+                otherBox.checked = false;
+            }
+        });
+
+        updateUI();
+    });
+});
+
+
+function updateUI() {
+
+    var text1 = document.getElementById("text");
+    var text2 = document.getElementById("text2");
+    var text3 = document.getElementById("text3");
+
+    text1.style.display = "none";
+    text2.style.display = "none";
+    text3.style.display = "none";
+
+    if (document.getElementById("myCheck").checked) {
+      text1.style.display = "block";
+      document.body.style.backgroundColor = "red";
+    } else if (document.getElementById("myCheck2").checked) {
+       text2.style.display = "block";
+       document.body.style.backgroundColor = "green";
+    } else if (document.getElementById("myCheck3").checked){
+        text3.style.display = "block";
+        document.body.style.backgroundColor = "blue";
+    } else {
+        document.body.style.backgroundColor = "";
+    }
+
+}
